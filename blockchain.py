ec827c39f1eb0d7a0a5b53d01ef8a2c41fc62adb
Grant Houston <grant.houston44@hotmail.com>
2021-02-20 18:08:54 +0000

    added comments to help understanding of oop (it's been a while)

--- blockchain.py
+++ blockchain.py
@@ -3,8 +3,16 @@ import hashlib
 import json
 from flask, import Flask, jsonify
 
+# blueprint for individual objects
+# we can create an object from a class
+# an object is an instance of the class - we instantiate an obect
 class Blockchain:
 
+    # self represents the instance of the class
+    # we use the self keyword to allow us to access the attributes and methods defined within the class
+     
+    # the init method is a constructor - this is called whenever an object is instantiated 
+    # in order to create the object
     def __init__(self):
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0')
