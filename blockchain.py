369bae41204206088e6bdb341f77b28eafdf6c3d
Grant Houston <grant.houston44@hotmail.com>
2021-02-20 17:53:44 +0000

    created blockchain class

--- blockchain.py
+++ blockchain.py
@@ -4,4 +4,7 @@ import json
 from flask, import Flask, jsonify
 
 class Blockchain:
-    
+
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
